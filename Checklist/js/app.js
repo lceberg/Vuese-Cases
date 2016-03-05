d544e6ed8a31d18ed2c2123e9d74082109f31d25
Kaloyan Ivanov <kaloyan.github@gmail.com>
2016-03-05 11:48:25 +0200

    Added Checklist example

--- Checklist/js/app.js
+++ Checklist/js/app.js
@@ -3,9 +3,44 @@
 	var Checklist = new Vue({
 
 		el: '#checklist',
-		
-		ready: function() {
-			
+
+		data: {
+			tasks: [],
+			taskName: ''
+		},
+
+		computed: {
+			progress: function() {
+				var tasksCount = this.tasks.length;
+				var tasksDone  = 0;
+
+				for ( task in this.tasks ) {
+					if ( ! this.tasks[task].done ) {
+						continue;
+					}
+
+					tasksDone++;
+				}
+
+				var percentage = Math.ceil( ( 100 / tasksCount ) * tasksDone );
+
+				return percentage > 100 ? 100 : percentage;
+			}
+		},
+
+		methods: {
+			addTask: function() {
+				this.tasks.push({
+					name: this.taskName,
+					done: false
+				});
+
+				this.taskName = '';
+			},
+
+			toggleTask: function(task) {
+				task.done = ! task.done;
+			}
 		}
 
 	});
